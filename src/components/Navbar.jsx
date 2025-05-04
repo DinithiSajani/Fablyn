579834364d02741b588bba930d0adced515d4c9e
DinithiSajani <dinithisajani@gmail.com>
2025-05-05 01:16:46 +0530

    update hero and logo images

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   const [visible,setVisible] = useState(false);
   return (
     <div className='flex items-center justify-between py-5 font-medium'>
-     <img src={assets.logo} className='w-36' alt="" />
+     <img src={assets.logo} className='w-60' alt="" />
     
      <ul className='hidden sm:flex gap-5 text-sm text-gray-700'>
 
