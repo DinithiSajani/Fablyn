209515188d7ceab9323ae00bdeaba7e283f2b704
DinithiSajani <dinithisajani@gmail.com>
2025-05-04 18:19:44 +0530

    adding the title- latest collections

--- src/components/LatestCollection.jsx
+++ src/components/LatestCollection.jsx
@@ -1,21 +1,17 @@
 import React, { useContext } from 'react'
 import { ShopContext } from '../context/ShopContext'
+import Title from './Title';
 
 const LatestCollection = () => {
- 
- const { products } = useContext(ShopContext);
+  const { products } = useContext(ShopContext);
 
- 
- 
- 
-    return (
+  return (
     <div className='my-10'>
-        <div className='text-center py-8 text-3xl'>
-
-        </div>
-
+      <div className='text-center py-8 text-3xl'>
+        <Title text1={'LATEST'} text2={'COLLECTIONS'} />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default LatestCollection
+export default LatestCollection;
