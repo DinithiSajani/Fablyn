d9583e51573c5c85aca22dae8291e3dcc4614d63
DinithiSajani <dinithisajani@gmail.com>
2025-05-04 17:19:20 +0530

    update

--- src/components/LatestCollection.jsx
+++ src/components/LatestCollection.jsx
@@ -2,10 +2,19 @@ import React, { useContext } from 'react'
 import { ShopContext } from '../context/ShopContext'
 
 const LatestCollection = () => {
-const { products } = useContext(ShopContext);
+ 
+ const { products } = useContext(ShopContext);
 
-  return (
-    <div>LatestCollection</div>
+ 
+ 
+ 
+    return (
+    <div className='my-10'>
+        <div className='text-center py-8 text-3xl'>
+
+        </div>
+
+    </div>
   )
 }
 
