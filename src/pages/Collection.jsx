ddd74290ae6f211852b73b494401b7bf3925ede6
DinithiSajani <dinithisajani@gmail.com>
2025-05-10 16:41:33 +0530

    complete search bar

--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -5,7 +5,7 @@ import Title from '../components/Title';
 import ProductItem from '../components/ProductItem';
 
 const Collection = () => {
-  const { products } = useContext(ShopContext);
+  const { products , search, showSearch } = useContext(ShopContext);
   const [showFilter, setShowFilter] = useState(false);
   const [filterProducts, setFilterProducts] = useState([]);
   const [category, setCategory] = useState([]);
@@ -40,6 +40,14 @@ const Collection = () => {
 
   const applyFilter = () => {
     let productsCopy = [...normalizedProducts];
+
+if (showSearch && search) {
+  productsCopy = productsCopy.filter(item => item.name.toLowerCase().includes(search.toLowerCase()))
+  
+}
+
+
+
 
     if (category.length > 0) {
       productsCopy = productsCopy.filter((item) =>
@@ -87,7 +95,7 @@ switch (sortType) {
 
   useEffect(() => {
     applyFilter();
-  }, [category, subCategory, sortOrder, normalizedProducts]);
+  }, [category, subCategory, sortOrder, normalizedProducts, search, showSearch]);
 
 
 useEffect(()=>{
