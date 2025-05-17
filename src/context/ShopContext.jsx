9f978261249806eac43ff619c458f57264a268b0
DinithiSajani <dinithisajani@gmail.com>
2025-05-17 19:41:27 +0530

    add to cart button with selectng size

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { products } from "../assets/assets";
 
 export const ShopContext = createContext();
@@ -8,6 +8,31 @@ const ShopContextProvider = (props) => {
   const delivery_fee = 10;
   const [search, setSearch] = useState('');
   const [showSearch, setShowSearch] = useState(false);
+  const [cartItems, setCartItems] = useState({});
+
+  const addToCart = async (itemId, size) => {
+    // Deep clone cartItems safely
+    let cartData = JSON.parse(JSON.stringify(cartItems));
+
+    // If the item already exists in the cart
+    if (cartData[itemId]) {
+      if (cartData[itemId][size]) {
+        cartData[itemId][size] += 1;
+      } else {
+        cartData[itemId][size] = 1;
+      }
+    } else {
+      // First time adding this item and size
+      cartData[itemId] = {};
+      cartData[itemId][size] = 1;
+    }
+
+    setCartItems(cartData);
+  };
+
+  useEffect(() => {
+    console.log(cartItems);
+  }, [cartItems]);
 
   const value = {
     products,
@@ -16,7 +41,9 @@ const ShopContextProvider = (props) => {
     search,
     setSearch,
     showSearch,
-    setShowSearch
+    setShowSearch,
+    cartItems,
+    addToCart
   };
 
   return (
